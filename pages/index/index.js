9a09360c42a0fab7664ca533f89172ff901ff609
Acinonyx Tungsten <gaoruihao@outlook.com>
2017-02-27 08:33:25 -0800

    初步做好了swiper

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,26 +1,43 @@
 //index.js
 //获取应用实例
 var app = getApp()
-Page({
+var pageObj={
   data: {
-    motto: 'Hello World',
-    userInfo: {}
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
-  }
-})
+     swiperItem:[],
+     text:false,
+     buttonclick:"buttonclick"
+    },
+
+ onLoad:function(){
+        this.addSwiperItem({src:"./res/slide1.jpg",func:"taps"},{src:"./res/slide2.jpg",func:"tap"},{src:"./res/slide3.jpg",func:"tap"});
+        console.log(this.swiperItem);
+        
+      },
+//绑定在控件上调试用
+      tap:function(event){
+        console.log("clicked");
+        wx.showToast({title:"You clicked me!"});
+        
+        setTimeout(wx.hideToast,1000);
+
+        
+      },
+
+//addSwiperItem方法用于在页面初始化时设定swiper的内容
+//接受不限制数量的表示swiper显示图片和绑定函数的对象
+//要不要改成绑定链接而不是绑定函数？
+//{src:"./res/slide1.jpg",func:"tap"}
+      addSwiperItem:function(){
+        
+        for(var i=0;i<arguments.length;i++){
+          this.data.swiperItem.push(arguments[i]);
+        }
+        this.setData({
+          swiperItem:this.data.swiperItem
+        });
+      }
+
+     
+
+}
+Page(pageObj)
