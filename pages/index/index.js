5c9e63adcbf82bfa0b31ef7055b7385093b588ba
Tungsten <gaoruihao@outlook.com>
2017-03-08 00:41:27 +0800

    认真思考了一下如何让模块真正模块化

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,17 +1,40 @@
 //index.js
 //获取应用实例
-var app = getApp()
+var app = getApp();
+var module =["template","test"];
+var testinsert=require("../../module/Template/template.js")
 var pageObj={
   data: {
      swiperItem:[],
      text:false,
-     buttonclick:"buttonclick"
+     buttonclick:"buttonclick",
+     modules:[{
+       id:1,
+       name:"template",
+       title:"从js渲染出来的模块1",
+       popnumber:["true","12"],
+       popdot:false,
+       subtitle:"这个模块是从js文件中渲染出来的啦~",
+       data:{index:"狼剩子小朋友每天都要坚持码代码~"}
+     },{
+       id:2,
+       name:"template",
+       title:"关于我们班运动会的口号",
+       subtitle:"我觉得这样比较好",
+       data:{index:"计一计一,宇宙第一,明年大二，今年大一"}
+     },{
+       id:0,
+       name:"template",
+       title:"召唤海通兽~",
+       subtitle:"海通兽也是你想召唤就能召唤的？！！",
+       popdot:true
+     }]
     },
 
  onLoad:function(){
         this.addSwiperItem({src:"./res/slide1.jpg",func:"taps"},{src:"./res/slide2.jpg",func:"tap"},{src:"./res/slide3.jpg",func:"tap"});
         console.log(this.swiperItem);
-        
+        console.log(testinsert.data.name);
       },
 //绑定在控件上调试用
       tap:function(event){
