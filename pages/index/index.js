7a20deea7cfe949ed3e3693a1cfbf42c85860732
rikumi <riku.mi@outlook.com>
2017-04-19 07:39:32 +0800

    x

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,83 +1,8 @@
-//index.js
-//获取应用实例
-var app = getApp();
-var module =["template","test"];
-var testinsert=require("../../module/Template/template.js")
-var pageObj={
+Page({
   data: {
-     swiperItem:[],
-     text:false,
-     buttonclick:"buttonclick",
-     modules:[{
-       id:1,
-       name:"template",
-       title:"从js渲染出来的模块",
-       popnumber:["true","1232"],
-       popdot:false,
-       subtitle:"男默女泪，金星看了默默流泪",
-       data:{index:"海通兽大哥哥最帅啦"}
-     }]
-    },
 
- onLoad:function(){
-        this.addSwiperItem({src:"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1488956552501&di=18842dd1a011655d06b54f13ecb080ab&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fforum%2Fw%253D580%2Fsign%3De5af69b9700e0cf3a0f74ef33a44f23d%2F7bd8128b87d6277febd2f6932a381f30eb24fcb0.jpg",func:"tap"});
-        console.log(this.swiperItem);
-        console.log(testinsert.data.name);
+  },
+  onLoad: function(options) {
 
-      },
-//绑定在控件上调试用
-      tap:function(event){
-        console.log("clicked");
-        wx.showToast({title:"You clicked me!"});
-        wx.navigateTo({
-          url: 'http://my.seu.edu.cn/',
-          success: function(res){
-            // success
-          },
-          fail: function() {
-            // fail
-          },
-          complete: function() {
-            // complete
-          }
-        });
-        setTimeout(wx.hideToast,1000);
-
-        
-      },
-
-//addSwiperItem方法用于在页面初始化时设定swiper的内容
-//接受不限制数量的表示swiper显示图片和绑定函数的对象
-//要不要改成绑定链接而不是绑定函数？
-//{src:"./res/slide1.jpg",func:"tap"}
-      addSwiperItem:function(){
-        
-        for(var i=0;i<arguments.length;i++){
-          this.data.swiperItem.push(arguments[i]);
-        }
-        this.setData({
-          swiperItem:this.data.swiperItem
-        });
-      },
-
-      addModule:function () {
-          for(var i=0;i<arguments.length;i++){
-              var tempModule={
-                  id:0,
-                  name:arguments[i].name,
-                  title:arguments[i].title,
-                  subtitle:arguments[i].subtitle,
-
-              }
-              this.data.modules.push(tempModule);
-
-          }
-
-          this.setData({
-              modules:this.data.modules
-          });
-      }
-     
-
-}
-Page(pageObj)
+  }
+})
---
+++ utils/wx.js
@@ -0,0 +1,185 @@
+// Wx.js 配置项
+var config = {
+  urlFormatter: route => 'https://www.heraldstudio.com/' + route,
+  pageFormatter: pageName => '/pages/' + pageName.split('?')[0] + '/' + pageName,
+  stopPoints: []
+}
+
+// 注入对象：把 injector 中的成员注入到 injectee
+// 若 replace == true，有冲突时，优先保留新注入的 injector 成员，原来 injectee 的成员名前加 super_ 作为备选
+// 若 replace == false，有冲突时，优先保留原有的 injectee 成员，新注入的成员名前加 super_ 作为备选
+function injectObject(injector, injectee, replace = false, recursive = true) {
+  for (var key in injector) {
+    // 若已存在原版成员，根据 replace 进行相应处理
+    try {
+      if (injectee[key]) {
+        if (recursive && typeof(injectee[key]) === 'object') {
+          injectObject(injector[key], injectee[key], replace, true)
+        } else if (replace) {
+          injectee['super_' + key] = injectee[key]
+          injectee[key] = injector[key]
+        } else {
+          injectee['super_' + key] = injector[key]
+        }
+      } else {
+        // 注入新版成员
+        injectee[key] = injector[key]
+      }
+    } catch (err) {
+      console.error('Wx.js Inject Fail:', key);
+    }
+  }
+}
+
+// Wx.js 核心注入
+function beginInject() {
+  // 注入 wx 成员
+  wx.$ = {}
+  // 注入 wx.$.util
+  wx.$.util = js => require(js + '.js')
+  injectObject(module.exports, wx.$, true)
+  // 注入 Page
+  wx.$.super_Page = Page
+  Page = _Page
+  // 测试注入结果
+  Page()
+}
+
+// Wx.js Page() 方法注入兼注入情况测试
+function _Page(obj) {
+  if (obj) {
+    var pageProto = wx.$.util('page')
+    injectObject(pageProto, obj, false)
+
+    wx.$.super_Page(obj)
+  } else { // obj 为空时展示注入情况
+    wx.$.log('Wx.js', 'Inject Success', 'Using Domain:', config.urlFormatter('[route]'))
+  }
+}
+
+// Pretty Log
+function log(title, status, obj1, obj2, obj3, obj4) {
+  console.log(
+    '%c' + title + '%c' + status,
+    "display: block; color: #fff; background: #1cadcf; border-radius: 2px 0 0 2px; padding:2px 5px; line-height: 20px",
+    "display: block; color: #fff; background: #88b46a; border-radius: 0 2px 2px 0; padding:2px 5px; line-height: 20px",
+    obj1 || '', obj2 || '', obj3 || '', obj4 || ''
+  )
+}
+
+// Pretty Log
+function error(title, status, obj1, obj2, obj3, obj4) {
+  console.error(
+    '%c' + title + '%c' + status,
+    "display: block; color: #fff; background: #1cadcf; border-radius: 2px 0 0 2px; padding:2px 5px; line-height: 20px",
+    "display: block; color: #fff; background: #88b46a; border-radius: 0 2px 2px 0; padding:2px 5px; line-height: 20px",
+    obj1 || '', obj2 || '', obj3 || '', obj4 || ''
+  )
+}
+
+/*
+  根据传入的操作列表，筛选出可执行的操作进行展示
+  @param actions: [
+    {
+      name: '操作名称',
+      condition: 操作可执行的条件,
+      action: 执行该操作的回调函数
+    }, ...
+  ]
+*/
+function showActions(actions) {
+  actions = actions.filter(k => k.condition != false)
+  if (actions.length) {
+    wx.showActionSheet({
+      itemList: actions.map(k => k.name),
+      success: function(data) {
+        if (typeof data.tapIndex === "number") {
+          actions[data.tapIndex].action()
+        }
+      }
+    })
+  } else {
+    wx.showActionSheet({
+      itemList: ['暂无相关操作'],
+      itemColor: '#888888'
+    })
+  }
+}
+
+function ask(title, message, callback) {
+  wx.showModal({
+    title: title,
+    content: message,
+    success: function(res) {
+      if (res.confirm) {
+        callback()
+      }
+    }
+  })
+}
+
+function requestApi(obj) {
+  obj.data.uuid = getApp().storage.uuid || '0000000000000000000000000000000000000000'
+  requestCompat(obj)
+}
+
+function requestCompat(obj) {
+  obj.header = obj.header || {}
+  obj.header['Content-Type'] = 'application/x-www-form-urlencoded'
+  wx.request({
+    url: obj.route ? config.urlFormatter(obj.route) : obj.url,
+    data: obj.data,
+    header: obj.header,
+    method: obj.method || 'GET',
+    success: function(res) {
+      obj.success && obj.success(res)
+    },
+    fail: function(res) {
+      obj.fail && obj.fail(res)
+    },
+    complete: function(res) {
+      if (res.statusCode < 400) {
+        wx.$.log(
+          res.statusCode, (obj.method || 'GET') + ' ' + obj.route,
+          'Data:', obj.data || 'none',
+          'Result: ', res
+        );
+      } else {
+        wx.$.error(
+          res.statusCode, (obj.method || 'GET') + ' ' + obj.route,
+          'Data:', obj.data || 'none',
+          'Response: ', res
+        );
+      }
+      obj.complete && obj.complete(res)
+    }
+  })
+}
+
+function showSuccess(str, callback) {
+  wx.showToast({ icon: 'success', title: str, success: callback })
+}
+
+function showLoading(str) {
+  wx.showToast({ icon: 'loading', title: str, duration: 10000 })
+  wx.showNavigationBarLoading()
+}
+
+function hideLoading() {
+  wx.hideToast()
+  wx.hideNavigationBarLoading()
+}
+
+function showError(str, callback) {
+  wx.showModal({ title: '提示', content: str, showCancel: false, success: callback })
+}
+
+function checkRegister() {
+  log('Wx.js', 'checkRegister', 'Not implemented')
+}
+
+module.exports = {
+  config, beginInject, Page, log, error,
+  ask, requestApi, requestCompat,
+  showActions, showSuccess, showLoading, hideLoading, showError, checkRegister
+}
