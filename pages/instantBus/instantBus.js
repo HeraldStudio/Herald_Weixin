cdff2f4e863f47f39af840feb345454cb224b75c
Vhyme <vhyme@live.cn>
2017-08-30 15:20:06 +0800

    x

--- pages/instantBus/instantBus.js
+++ pages/instantBus/instantBus.js
@@ -1,4 +1,59 @@
 const busService = require('../../providers/busService.js')
+const polylineData = [
+  [
+    "118.82113|31.89210",
+    "118.82113|31.89111",
+    "118.81727|31.89092",
+    "118.81448|31.89073",
+    "118.81423|31.89069",
+    "118.81408|31.89063",
+    "118.81398|31.89050",
+    "118.81390|31.89032",
+    "118.81379|31.88882",
+    "118.81379|31.88857",
+    "118.81404|31.88511",
+    "118.81410|31.88388",
+    "118.81412|31.88364",
+    "118.81455|31.88245",
+    "118.81473|31.88230",
+    "118.81505|31.88215",
+    "118.81537|31.88212",
+    "118.82206|31.88250",
+    "118.82437|31.88279",
+    "118.82455|31.88286",
+    "118.82501|31.88575",
+    "118.82501|31.88593",
+    "118.82483|31.88792",
+    "118.82321|31.89359",
+    "118.82321|31.89374",
+    "118.82331|31.89387",
+    "118.83006|31.89437",
+    "118.83006|31.89485"
+  ],
+  [
+    "118.81378|31.88362",
+    "118.81313|31.88349",
+    "118.81226|31.88342",
+    "118.81202|31.88466",
+    "118.81203|31.88476",
+    "118.81208|31.88486",
+    "118.81216|31.88496",
+    "118.81228|31.88510",
+    "118.81226|31.88629",
+    "118.81117|31.88624",
+    "118.81163|31.88346",
+    "118.81284|31.87973",
+    "118.81426|31.87574",
+    "118.81516|31.87412",
+    "118.81847|31.86901",
+    "118.81864|31.86895",
+    "118.81878|31.86892",
+    "118.81925|31.86896",
+    "118.81954|31.86903",
+    "118.82308|31.86921",
+    "118.82306|31.86971"
+  ]
+]
 
 Page({
   data: {
@@ -52,10 +107,10 @@ Page({
             longitude: k.station.longitude,
             callout: {
               content: k.station.name,
-              color: '#333333',
+              color: '#ffffff',
               fontSize: 14,
               borderRadius: 5,
-              bgColor: '#ffffff',
+              bgColor: '#3388ff',
               padding: 10
             },
             iconPath: '/images/icon_station.png',
@@ -64,30 +119,17 @@ Page({
             anchor: { x: 0.5, y: 1 }
           }
         }),
-        lines: buses
-          .filter(i => i.id % 2 === 0)
-          .map(i => i.linePoints
-            .map(k => k.parentIds.map(i => [i, k.id]))
-            .reduce((a, b) => a.concat(b), []).map(k => {
-
-              let fromStop = i.linePoints.filter(i => i.id === k[0])
-              if (fromStop.length !== 1) {
-                return null
-              }
-              fromStop = fromStop[0]
-              let toStop = i.linePoints.filter(i => i.id === k[1])
-              if (toStop.length !== 1) {
-                return null
-              }
-              toStop = toStop[0]
-              return {
-                points: [fromStop.station, toStop.station],
-                color: '#3388ff80',
-                dottedLine: true,
-                width: 2
-              }
-            }).filter(k => k !== null)
-          ).reduce((a, b) => a.concat(b)),
+        lines: polylineData.map(i => i.map((k, index) => index && {
+          points: [ {
+            longitude: parseFloat(i[index - 1].split('|')[0]),
+            latitude: parseFloat(i[index - 1].split('|')[1])
+          }, {
+            longitude: parseFloat(k.split('|')[0]),
+            latitude: parseFloat(k.split('|')[1])
+          } ],
+          color: '#3388ff40',
+          width: 2
+        }).slice(1)).reduce((a, b) => a.concat(b), []),
         buses: buses
           .map(i => i.buses
             .map(k => {
