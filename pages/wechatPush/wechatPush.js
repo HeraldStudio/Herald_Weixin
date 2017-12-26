d1330921c896275d2fa20a96d9a92cad71e27ec2
rikumi <riku.mi@outlook.com>
2017-12-27 00:31:40 +0800

    课表预测

--- pages/wechatPush/wechatPush.js
+++ pages/wechatPush/wechatPush.js
@@ -16,9 +16,30 @@ Page({
           that.setData({ title: titleMatch[1] })
         }
 
+        let authorMatch = /[^.]rich_media_meta_nickname[^>]*>\s*([^<]*)\s*</img.exec(res.data)
+        if (authorMatch) {
+          that.setData({ author: authorMatch[1] })
+        }
+
+        let authorIdMatch = /微信号[\s\S]*?profile_meta_value">([^<]*)</img.exec(res.data)
+        if (authorIdMatch) {
+          that.setData({ authorId: authorIdMatch[1] })
+        }
+
+        let qrcodeMatch = /var msg_link = "(.+)";/.exec(res.data)
+        if (qrcodeMatch) {
+          qrcodeMatch = qrcodeMatch[1].replace(/\\x26amp;/g, '&')
+          qrcodeMatch = /(__biz=.*?)&chksm=/.exec(qrcodeMatch)
+          if (qrcodeMatch) {
+            qrcodeMatch = 'https://mp.weixin.qq.com/mp/qrcode?scene=10000004&size=102&' + qrcodeMatch
+            that.setData({ qrcode: qrcodeMatch })
+          }
+        }
+
         let html = res.data
           .replace(/<!([^<>]*<[^<>]*>)*[^<>]*>/g, '')
           .replace(/<(script|style|title|h2)[\s\S]*?<\/\s*\1>/img, '')
+          .replace(/<(iframe)[\s\S]*?<\/\s*\1>/img, '<p style="font-size: 12px">小程序不支持此控件，请到文末复制链接打开</p>')
           .replace(/(<div\s+id="meta_content"[^>]*>)[\s\S]*?(<div\s+class="rich_media_content)/img, '$1$2')
           .replace(/([>^])\s+([<$])/g, '$1$2')
           .replace(/[\r\n]/g, '')
@@ -28,7 +49,7 @@ Page({
           .replace(/(<img[^>]+)data-src=/img, '$1 src=')
           .replace(/(<img[^>]+style=")([^>]+>)/img, '$1max-width: 100%;$2')
           .replace(/(<img[^>]*?)(\/?>)/img, '$1 style="max-width: 100%;"$2')
-          .replace(/width:\s*([\d\.]+px|auto)(;?)/img, 'width: 100%;$2')
+          // .replace(/width:\s*([\d\.]+px|auto)(;?)/img, 'max-width: 100%;$2')
           .replace(/(width|height)="[^"]+"/img, '')
           .replace(/<a[^>]*?>阅读原文<\/\s*a>/img, '')
 
